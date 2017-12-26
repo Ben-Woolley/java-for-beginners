f93917d3c95e021be389ad991f4b84d33eb3e509
Benjamin Woolley <bsw22@bath.ac.uk>
2017-12-26 11:25:44 +0000

    resolved merge conflicts

--- pokemon-code-academy/src/code/academy/WildPokemon.java
+++ pokemon-code-academy/src/code/academy/WildPokemon.java
@@ -9,6 +9,12 @@ class WildPokemon
     static final String DITTO = "Ditto";
     static final String SNORLAX = "Snorlax";
 
+    static Pokemon pidgey = new Pokemon(PIDGEY, 30, 12);
+    static Pokemon caterpie = new Pokemon(CATERPIE, 50, 30);
+    static Pokemon magikarp = new Pokemon(MAGIKARP, 27, 8);
+    static Pokemon ditto = new Pokemon(DITTO, 25, 10);
+    static Pokemon snorlax = new Pokemon(SNORLAX, 15, 5);
+
     /**
      * Given a random number
      *
