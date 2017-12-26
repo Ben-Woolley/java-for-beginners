1fb030e914b94993cd55303ad8a71f58680ea60d
Benjamin Woolley <bsw22@bath.ac.uk>
2017-12-26 11:01:56 +0000

    Remove non-compiling pokemon instantiations

--- pokemon-code-academy/src/code/academy/WildPokemon.java
+++ pokemon-code-academy/src/code/academy/WildPokemon.java
@@ -1,7 +1,5 @@
 package code.academy;
 
-import code.academy.Pokemon;
-
 class WildPokemon
 {
 
@@ -10,12 +8,6 @@ class WildPokemon
     static final String MAGIKARP = "Magikarp";
     static final String DITTO = "Ditto";
     static final String SNORLAX = "Snorlax";
-
-    static Pokemon pidgey = new Pokemon(PIDGEY, 30, 12);
-    static Pokemon caterpie = new Pokemon(CATERPIE, 50, 30);
-    static Pokemon magikarp = new Pokemon(MAGIKARP, 27, 8);
-    static Pokemon ditto = new Pokemon(DITTO, 25, 10);
-    static Pokemon snorlax = new Pokemon(SNORLAX, 15, 5);
 
     /**
      * Given a random number
