fef7cfbe384a63daace05dad6a6c5774058a636d
Benjamin Woolley <bsw22@bath.ac.uk>
2017-12-26 12:59:32 +0000

    Add sillyEquation example class

---
+++ pokemon-code-academy/src/lessons/lesson1/basic_java_concepts/SillyEquation.java
@@ -0,0 +1,10 @@
+package lessons.lesson1.basic_java_concepts;
+
+public class SillyEquation {
+    public static void main(String[] args) {
+        int that = 2;
+        int theOther = 4;
+        int result = that + theOther;
+        System.out.println("The result is " + result);
+    }
+}
