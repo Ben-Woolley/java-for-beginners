d4255376da6c284dd88fa3a95e99000be21e18a5
Ben Woolley <ben.s.woolley@gmail.com>
2020-03-07 10:56:32 +0000

    delete tests, not valuable for beginners

--- intro-to-java/src/main/java/eca/exercises/OOEV/answers/AnswersOOEV.java
+++ intro-to-java/src/main/java/eca/exercises/OOEV/answers/AnswersOOEV.java
@@ -2,14 +2,17 @@ package eca.exercises.OOEV.answers;
 
 public class AnswersOOEV {
 
-    public static Boolean isOdd(Integer number) {
-        // A number is odd if it has a remainder when divided by 2
-        Boolean isOdd = number % 2 == 1;
-        return isOdd;
-    }
-
-
     public static void main(String[] args) {
-        System.out.println(isOdd(4));
+        // Pick a number
+        Integer number = 10;
+        // Determine whether your number is odd or even
+        Boolean isEven = number % 2 == 0;
+
+        // Print the result
+        if (isEven) {
+            System.out.println(number + " is even");
+        } else {
+            System.out.println(number + " is odd");
+        }
     }
 }
